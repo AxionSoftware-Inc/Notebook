2c575572d8b112333e10eaca6f035a9924222380
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-08-27 20:31:07 +0500

    Load notebook premium styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Manrope, Playfair_Display } from "next/font/google";
 
 import "./globals.css";
+import "./notebook.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 const manrope = Manrope({
